21d6de794117da5e9757ffad2f75e6999c4dbd8a
cuellogabriel <robertogabrielcuello@gmail.com>
2025-03-13 18:15:48 -0300

    primeros pasos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Smooth Scroll para navegación
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
